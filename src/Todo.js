7902c350825442e670220a034372bead104259db
marsespinosa <mariconespinosa888@gmail.com>
2021-12-27 10:09:47 +0800

    ver1

---
+++ src/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function todo({todo}) {
+    return (
+        <div>
+            <label>
+            <input type="checkbox" checked={todo.complete} />
+            {todo.name}
+            </label>
+        </div>
+    )
+}
